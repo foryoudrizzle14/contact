b1d486a9087937ec58c301575a254f37856f4595
foryoudrizzle141~ <foryoudrizzle14@gmail.com>
2023-10-27 20:59:25 +0900

    erasing import react

--- src/index.js
+++ src/index.js
@@ -6,8 +6,4 @@ import ReactDOM from "react-dom/client";
 import ContactIndex from "./Components/ContactPages/ContactIndex";
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
-root.render(
-  <div>
-    <ContactIndex />
-  </div>
-);
+root.render(<ContactIndex />);
