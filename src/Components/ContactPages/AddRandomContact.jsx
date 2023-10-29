e26653b334a517ac15b67466fbd29770ce76a73e
foryoudrizzle141~ <foryoudrizzle14@gmail.com>
2023-10-29 19:05:36 +0900

    adding api

--- src/Components/ContactPages/AddRandomContact.jsx
+++ src/Components/ContactPages/AddRandomContact.jsx
@@ -1,7 +1,22 @@
-const AddRandomContact = () => {
+import { getRandomUser } from "../../Utility/api";
+
+const GetRandomContact = async props => {
+  const responseFromAPI = await getRandomUser();
+  console.log(responseFromAPI);
+
+  return props.handleAddRandomContact({
+    name: responseFromAPI.data.first_name + " " + responseFromAPI.data.last_name,
+    email: responseFromAPI.data.email,
+    phone: responseFromAPI.data.phone_number,
+  });
+};
+
+const AddRandomContact = props => {
   return (
     <div>
-      <button className="btn btn-danger form-control">Add Random Contact</button>
+      <button className="btn btn-success form-control" onClick={() => GetRandomContact(props)}>
+        Add Random Contact
+      </button>
     </div>
   );
 };
