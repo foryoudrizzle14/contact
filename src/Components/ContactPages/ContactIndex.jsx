43772b3930e0612ce76bde8d2ada71bfe028f911
foryoudrizzle141~ <foryoudrizzle14@gmail.com>
2023-10-29 20:04:06 +0900

    update contact page

--- src/Components/ContactPages/ContactIndex.jsx
+++ src/Components/ContactPages/ContactIndex.jsx
@@ -15,21 +15,21 @@ class ContactIndex extends React.Component {
       contactList: [
         {
           id: 1,
-          name: "Ben Parker",
+          name: "홍길동",
           phone: "666-666-7770",
           email: "ben@dotnetmastery.com",
           isFavorite: false,
         },
         {
           id: 2,
-          name: "Kathy Patrick",
+          name: "이영수",
           phone: "111-222-0000",
           email: "kathy@dotnetmastery.com",
           isFavorite: true,
         },
         {
           id: 3,
-          name: "Paul Show",
+          name: "이영미",
           phone: "999-222-1111",
           email: "paul@dotnetmastery.com",
           isFavorite: true,
--- src/Components/ContactPages/RemoveAllContact.jsx
+++ src/Components/ContactPages/RemoveAllContact.jsx
@@ -1,9 +1,9 @@
-import react from "react";
-
-const RemoveAllContact = () => {
+const RemoveAllContact = props => {
   return (
     <div>
-      <button className="btn btn-danger form-control">Remove All</button>
+      <button className="btn btn-danger form-control" onClick={() => props.handleRemoveAllContact()}>
+        Remove All
+      </button>
     </div>
   );
 };
